358f758fdb11593e0352141c20c1202b3efa51dc
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-09-03 10:32:59 -0300

    Add cron job to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/:urlId'],
+  publicRoutes: ['/', '/:urlId', '/api/cron/check-expired-urls'],
 });
 
 export const config = {
