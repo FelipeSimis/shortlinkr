9271ae61acd60c32f94ebf488413ce244c46c605
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-12-11 17:35:47 -0300

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.google.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+      }
+    ]
   },
-  experimental: {
-    serverActions: true,
-  }
 }
 
 module.exports = nextConfig
