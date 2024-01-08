960af4dd7ddc50ccf8c60d811f3aba2fe7a27978
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-01-08 18:43:24 -0300

    perf: optimize cache configuration for getUrlInfo

--- src/helpers/getUrlInfo.ts
+++ src/helpers/getUrlInfo.ts
@@ -3,21 +3,28 @@ import { unstable_cache as cache } from 'next/cache';
 
 import prismaDb from '@/lib/prismaDb';
 
-export const getUrlInfo = cache(async (urlId: string) => {
-  const url = await prismaDb.longUrl.findFirst({
-    where: {
-      id: urlId,
-    },
-    select: {
-      shortUrl: true,
-      isActive: true,
-      expirationDate: true,
-    },
-  });
+export const getUrlInfo = cache(
+  async (urlId: string) => {
+    const url = await prismaDb.longUrl.findFirst({
+      where: {
+        id: urlId,
+      },
+      select: {
+        shortUrl: true,
+        isActive: true,
+        expirationDate: true,
+      },
+    });
 
-  return {
-    shortUrl: url?.shortUrl,
-    isActive: url?.isActive,
-    expirationDate: url?.expirationDate,
-  };
-});
+    return {
+      shortUrl: url?.shortUrl,
+      isActive: url?.isActive,
+      expirationDate: url?.expirationDate,
+    };
+  },
+  ['get-url-info'],
+  {
+    revalidate: 10,
+    tags: ['url-info'],
+  },
+);
