6d1e082c6fe3afd8ea5b0daaf9804a2fa86c4b7f
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-01-08 18:27:53 -0300

    perf: optimize cache configuration for getCurrentUserShortURLs

--- src/helpers/getCurrentUserUrls.ts
+++ src/helpers/getCurrentUserUrls.ts
@@ -1,37 +1,43 @@
 import 'server-only';
-import { auth } from '@clerk/nextjs/server';
+import { unstable_cache as cache } from 'next/cache';
 
 import prismaDb from '@/lib/prismaDb';
 
 import { ITEMS_PER_PAGE } from '@/constants';
 
 type Props = {
+  userId: string;
   currentPage?: number;
 };
 
-export const getCurrentUserShortURLs = async ({ currentPage = 1 }: Props) => {
-  const { userId } = auth();
+export const getCurrentUserShortURLs = cache(
+  async ({ userId, currentPage = 1 }: Props) => {
+    const [urls, totalUrls] = await prismaDb.$transaction([
+      prismaDb.longUrl.findMany({
+        where: {
+          userId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        skip: (currentPage - 1) * ITEMS_PER_PAGE,
+        take: ITEMS_PER_PAGE,
+      }),
+      prismaDb.longUrl.count({
+        where: {
+          userId,
+        },
+      }),
+    ]);
 
-  const [urls, totalUrls] = await prismaDb.$transaction([
-    prismaDb.longUrl.findMany({
-      where: {
-        userId,
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-      skip: (currentPage - 1) * ITEMS_PER_PAGE,
-      take: ITEMS_PER_PAGE,
-    }),
-    prismaDb.longUrl.count({
-      where: {
-        userId,
-      },
-    }),
-  ]);
-
-  return {
-    urls,
-    totalUrls,
-  };
-};
+    return {
+      urls,
+      totalUrls,
+    };
+  },
+  ['get-current-user-urls'],
+  {
+    revalidate: 10,
+    tags: ['current-user-urls'],
+  },
+);
