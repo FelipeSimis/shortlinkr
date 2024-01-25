09e31b32844501d00def7347f9c9c0e6f9e02f6a
Felipe Simis <felipee.oliveira17@hotmail.com>
2024-01-25 12:45:06 -0300

    chore: add metadata to SignIn and SignUp pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import { SignIn } from '@clerk/nextjs';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+  description: 'Sign in to your Shortlinkr account',
+};
 
 const SignInPage = () => {
   return <SignIn signUpUrl="/sign-up" />;
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import { SignUp } from '@clerk/nextjs';
+
+export const metadata: Metadata = {
+  title: 'Sign Up',
+  description: 'Sign up to create a Shortlinkr account',
+};
 
 const SignUpPage = () => {
   return <SignUp signInUrl="/sign-in" />;
